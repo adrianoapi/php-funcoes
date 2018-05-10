ffde74737770a46f19733b9ad0b615df07c0f963
adrianometalslug <sdcomputadores@gmail.com>
2018-05-10 15:31:03 -0300

    update strings

--- functions.php
+++ functions.php
@@ -6,6 +6,19 @@
 # Funcoes nativas do PHP
 ------------------------------------------------------------------------------*/
 
+
+# 5.1	Creating and Accessing Strings
+
+#\n	A line feed character (ASCII 10)
+#\n	A carriage return character (ASCII 13)
+#\t	A horizontal tab character (ASCII 9)
+#\v	A vertical tab character (ASCII 11)
+#\f	A form feed character (ASCII 12)
+#\\	A backslash (as opposed to the start of an escape sequence)
+#\$	A $ symbol (as opposed to the start of a variable name)
+#\"	A double quote (as opposed to the double quote marking the end of a string)
+
+
 # substr
 $myString = "Hello, world!";
 echo substr( $myString, 0, 5   ) . "<br/>"; // Displays 'Hello'
@@ -28,9 +41,13 @@ echo "<hr>";
 $myString = "Hello, world!";
 echo strstr( $myString, "wor" ) . "<br/>";                    // Displays 'world!'
 echo ( strstr( $myString, "xtz" ) ? "Yes" : "No" ) . "<br/>"; // Displays 'No'
-echo strstr( $myString, "wor", true );                        // Displays 'Hello,'
 # Returns the portion from the start of the string to the character before the found text:
-
+echo strstr( $myString, "wor", true ) . "<br/>";              // Displays 'Hello,'
+echo strpos( $myString, "wor" ) . "<br/>";                    // Displays '7'
+echo var_dump(strpos( $myString, "xyz" ));                    // Displays '' (false)
+echo !strpos( $myString, "Hel" ) ? "Not found <br/>" : TRUE;  // Displays 'Not found'
+echo strpos( $myString, "o" ) . "<br />";  // Displays '4'
+echo strrpos( $myString, "o" ) . "<br />"; // Displays '8'
 echo "<hr>";
 
 $religion = 'Hebrew';
