70a3020ee2f70f1a3d3dc3c04b1391f14dd39fe6
adrianometalslug <sdcomputadores@gmail.com>
2018-05-24 10:06:18 -0300

    add xor functions, add AND

---
+++ xor.php
@@ -0,0 +1,25 @@
+<?php
+
+/*-----------------------------------------------------------------------------
+# PHP FUNCTIONS
+# -----------------------------------------------------------------------------
+# XOR
+------------------------------------------------------------------------------*/
+
+/*
+ * Operador & ( Bitwise AND )
+ * O operador & ( Bitwise AND ) compara dois valores utilizando suas representações
+ * binárias, retornando um novo valor, para formar esse valor de retorno cada bit é comparado,
+ * retornando 1( true ) [quando ambos os bits forem iguais] a 1( true ),
+ * caso contrário retorna 0( false ).
+ */
+
+$a = 5;
+$b = 1;
+echo $a & $b;
+//   00000101
+// & 00000001
+//-----------
+//   00000001 => 1 DECIMAL
+
+
