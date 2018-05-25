7a2f6263adc6c153d3a70fc61b250a1858f2fa0e
adrianometalslug <sdcomputadores@gmail.com>
2018-05-25 09:37:27 -0300

    update comment array

--- array.php
+++ array.php
@@ -5,12 +5,12 @@ require '_config.php';
 $a = "Original";
 $my_array = array("a" => "Cat","b" => "Dog", "c" => "Horse");
 extract($my_array);
-debug("\$a = $a; \$b = $b; \$c = $c \n");                                         // $a = Cat; $b = Dog; $c = Horse
+debug("\$a = $a; \$b = $b; \$c = $c \n");                                       // $a = Cat; $b = Dog; $c = Horse
 extract($my_array, EXTR_PREFIX_SAME, 'dup');
-debug("\$a = $a; \$b = $b; \$c = $c \$dup_b = $dup_b \n");                        // $a = Cat; $b = Dog; $c = Horse $dup_b = Dog  
+debug("\$a = $a; \$b = $b; \$c = $c \$dup_b = $dup_b \n");                      // $a = Cat; $b = Dog; $c = Horse $dup_b = Dog  
 $array = "Initially";
 $arr1 = array("a" => "Rose","b" => "Lotus", "c" => "Marigold");
 extract($arr1);
-debug("\$a = $a; \$b = $b; \$c = $c");
+debug("\$a = $a; \$b = $b; \$c = $c");                                          // $a = Rose; $b = Lotus; $c = Marigold
 
 ?>
