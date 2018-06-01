8642079d85b9fbf5d53987336478ac62a9f36e77
adrianoapi <adrianoapi@hotmail.com>
2018-06-01 14:20:15 -0300

    update vars

--- vars.php
+++ vars.php
@@ -40,3 +40,18 @@ function track() {
 track();
 track();
 track();
+
+echo "<hr>";
+
+echo <<<END_COMMENT
+/*
+ * Como o número inteiro é "300" não está no intervalo especificado e a saída do
+ * código acima será: "Integer is not valid".
+ */
+END_COMMENT;
+$var         = 300;
+$int_options = array( "options" => array ( "min_range" => 0, "max_range" => 256 ) );
+if (!filter_var( $var, FILTER_VALIDATE_INT, $int_options ))
+    echo("Integer is not valid");
+else
+    echo("Integer is valid");
