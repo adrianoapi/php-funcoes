3a5c0901df3c8c457788aef4e4d1c7023cde1fa4
adrianoapi <adrianoapi@hotmail.com>
2018-06-01 10:13:20 -0300

    update vars

--- vars.php
+++ vars.php
@@ -12,3 +12,31 @@ $bar = &$foo;
 $bar = "My name is $bar";  
 echo $bar;
 echo $foo;
+
+echo "<hr>";
+
+echo <<<END_COMMENT
+/*
+ * O php trata Strings da mesma forma que arrays, permitindo que caracteres específicos
+ * sejam acessados via notação offset de array.
+ */
+END_COMMENT;
+$color = "maroon";
+$var   = $color[2];
+echo "$var";
+
+echo "<hr>";
+
+echo <<<END_COMMENT
+/*
+ * Como var count é estático, ele retém seu valor anterior sempre que a função é executada.
+ */
+END_COMMENT;
+function track() {
+    static $count = 0;
+    $count++;
+    echo $count;
+}
+track();
+track();
+track();
