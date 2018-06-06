e815b55550e726bd0216b3ee5af71a5c2a979f43
adrianoapi <adrianoapi@hotmail.com>
2018-06-06 10:08:32 -0300

    add legend table array sort

--- array_sort.php
+++ array_sort.php
@@ -9,4 +9,25 @@ require '_config.php';
 ------------------------------------------------------------------------------*/
 
 
+/*------------------------------------------------------------------+
+| sorting arrays                                                    |
++-------------------------------------------------------------------+
+| sort() and rsort()   | For sorting indexed arrays                 |
++----------------------+--------------------------------------------+
+| asort() and arsort() | For sorting associative arrays             |
++----------------------+--------------------------------------------+
+| ksort() and krsort() | For sorting associative arrays by key      |
+|                      | rather than by value                       |
++----------------------+--------------------------------------------+
+| array_multisort()    | A powerful function that can sort multiple |
+|                      | arrays at once, or multidimensional arrays |
++----------------------+-------------------------------------------*/
+
+$games = array(
+    "Winning Eleven Final Version 3",
+    "Ridge Racer Type 4",
+    "Tony Hawk Pro Skater 2",
+    "Gran Turismo 2"
+);
+
 ?>
