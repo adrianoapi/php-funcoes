a551d88a5872937a4b187f7bf1f7dfe21ce5878b
adrianoapi <adrianoapi@hotmail.com>
2018-06-07 17:28:58 -0300

    add letras

--- regx.php
+++ regx.php
@@ -2,7 +2,7 @@
 
 require '_config.php';
 
-$letters = <<<END_TXT
+$thecure = <<<END_TXT
 [This Twilight Garden]
 I lift my lips from kissing you
 To kiss the sky
@@ -31,5 +31,92 @@ I am lost in you
 No-one will ever take your place
 So in love with you
 END_TXT;
+
+$offspring = <<<END_TXT
+Sometimes I think I'm gonna drown     | Às vezes eu acho que vou me afogar
+'Cause everyone around's so hollow    | Porque o mundo ao redor é tão oco
+I'm alone                             | Eu estou sozinho
+Sometimes I think I'm going down      | Às vezes eu acho que estou indo para baixo
+But no one makes a sound              | Mas ninguém faz um som
+They follow                           | Eles seguem
+And I'm alone                         | E eu estou sozinho
+Yeah if I make it I'd be amazed       | Sim, se eu fizer isso eu ficaria espantado
+Just to find tomorrow                 | Apenas para encontrar amanhã
+One more day and I'd be amazed        | Mais um dia e eu ficaria espantado
+Just to see it waiting                | Só para vê-lo esperando
+And if I make it I'm still alone      | E se eu fizer isso eu ainda estou sozinho
+No more hope for better days          | Não há mais esperança para dias melhores
+But if I could change
+Then I'd really be amazed
+And when you know you can't relate
+To one more shiny face
+Your heart breaks
+No one cares
+And when you know you can't go on
+'Cause everything is wrong
+Your heart breaks
+But no one's there
+END_TXT;
+        
+$guns = <<<END_TEXT
+She's got a smile it seems to me          | Ela tem um sorriso que parece com o meu
+Reminds me of childhood memories          | Lembra-me de memórias da infância
+Where everything
+Was as fresh as the bright blue sky
+Now and then when I see her face
+She takes me away to that special place
+And if I'd stare too long
+I'd probably break down and cry
+Oh, oh, oh
+Sweet child o' mine
+Oh, oh, oh, oh
+Sweet love of mine
+She's got eyes of the bluest skies
+As if they thought of rain
+I hate to look into those eyes
+And see an ounce of pain
+Her hair reminds me of a warm safe place
+Where as a child I'd hide
+And pray for the thunder
+And the rain
+To quietly pass me by
+Oh, oh, oh
+Sweet child o' mine
+Oh, oh, oh, oh
+Sweet love of mine
+Oh, oh, oh, oh
+Sweet child o' mine
+Oh, oh, oh, oh
+Sweet love of mine
+Oh, oh, oh, oh
+Sweet child o' mine
+Oh,
+Sweet love of mine
+Where do we go?
+Where do we go now?
+Where do we go?
+Oh, oh
+Where do we go?
+Oh,
+Where do we go now?
+Where do we go?
+Oh, (sweet child)
+Where do we go now?
+Oh,
+Where do we go now?
+Oh,
+Where do we go?
+Oh,
+Where do we go now?
+Oh,
+Where do we go?
+Where do we go now?
+Where do we go?
+Oh,
+Where do we go now?
+No, no, no, no, no, no
+Sweet child,
+Sweet child of mine
+END_TEXT;
 
 ?>
