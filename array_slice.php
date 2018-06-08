85264dc340ddb5c32e13c9f0726dca7811afc727
adrianoapi <adrianoapi@hotmail.com>
2018-06-08 08:54:01 -0300

    add description arrays functions

--- array_slice.php
+++ array_slice.php
@@ -8,6 +8,19 @@ require '_config.php';
 # Exemplos de manipulação de array
 ------------------------------------------------------------------------------*/
 
-
+/*-------------------------------------------------------------------------+
+| Adding and Removing Array Elements                                       |
++--------------------------------------------------------------------------+
+| array_unshift() | Adds one or more new elements to the start of an array |
++-----------------+--------------------------------------------------------+
+| array_shift()   | Removes the first element from the start of an array   |
++-----------------+--------------------------------------------------------+
+| array_push()    | Adds one or more new elements to the end of an array   |
++-----------------+--------------------------------------------------------+
+| array_pop()     | Removes the last element from the end of an array      |
++-----------------+--------------------------------------------------------+
+| array_splice()  | Removes element(s) from and/or adds element(s) to any  |
+|                 | point in an array                                      |
++-----------------+-------------------------------------------------------*/
 
 ?>
