a0ab09c1ebc6809cb17729e35ba3c24ae6ebbe43
adrianoapi <adrianoapi@hotmail.com>
2018-06-12 09:23:38 -0300

    usort with anonymous function

--- functions.php
+++ functions.php
@@ -54,4 +54,38 @@ echo "<br/>" . "I've counted to: " . nextNumber();
 echo "<br/>" . "I've counted to: " . nextNumber();
 echo "<br/>" . "I've counted to: " . nextNumber();
 
+echo "<hr>";
+
+echo <<<END_COMMENT
+/*
+ * Sorting words in a block of text by length
+ */
+END_COMMENT;
+
+$myText = <<<END_TEXT
+But think not that this famous town has
+only harpooneers, cannibals, and
+bumpkins to show her visitors. Not at
+all. Still New Bedford is a queer place.
+Had it not been for us whalemen, that
+tract of land would this day perhaps
+have been in as howling condition as the
+coast of Labrador.
+END_TEXT;
+
+echo "<h2>The text:</h2>";
+echo "<div style=\"width: 30em;\">$myText</div>";
+
+$myText = preg_replace( "/[\,\.]/", "", $myText );
+$words = array_unique( preg_split( "/[ \n\n\t]+/", $myText ) );
+usort( $words, create_function( '$a, $b', 'return strlen($a) - strlen($b); ' ) );
+
+echo "<h2>The sorted words:</h2>";
+echo "<div style=\"width: 30em;\">";
+
+foreach ( $words as $word ) {
+  echo "$word ";
+}
+ 
+echo "</div>";
 ?>
