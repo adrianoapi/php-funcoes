6a275df9127554990794ac1c6232c9bd7b148d62
adrianoapi <adrianoapi@hotmail.com>
2018-06-13 15:29:30 -0300

    update car_page

--- oo/car_page.php
+++ oo/car_page.php
@@ -1,14 +1,9 @@
-<?php
-
-require '../_config.php';
-
-?>
+<?php require '../_config.php'; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <title>Defining and Using Object Properties</title>
-    <link rel="stylesheet" type="text/css" href="common.css" />
   </head>
   <body>
     <h1>Defining and Using Object Properties</h1>
@@ -21,11 +16,11 @@ require '../_config.php';
     }
 
     $beetle = new Car();
-    $beetle->color = "red";
+    $beetle->color        = "red";
     $beetle->manufacturer = "Volkswagen";
 
     $mustang = new Car();
-    $mustang->color = "green";
+    $mustang->color        = "green";
     $mustang->manufacturer = "Ford";
 
     echo "<h2>Some properties:</h2>";
