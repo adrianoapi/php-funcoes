7e348c46d794dca8f67a8286393ff8485360b808
adrianoapi <adrianoapi@hotmail.com>
2018-06-13 15:19:17 -0300

    - Update debug function; - Add dir oo;

--- _config.php
+++ _config.php
@@ -5,7 +5,7 @@ header("Content-type: text/html; charset=iso-8859-1");
 function debug($data, $stop = false)
 {
     echo "<pre>";
-    if(is_array($data)){
+    if(is_array($data) || is_object($data)){
         print_r($data);
     }else{
         echo $data;
