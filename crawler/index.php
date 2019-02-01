7c888391fad769e3ecf3b9e900bf7ca01a93f272
adrianoapi <adrianoapi@hotmail.com>
2019-02-01 11:09:44 -0200

    add links

--- crawler/index.php
+++ crawler/index.php
@@ -3,13 +3,25 @@
 
 require_once 'Crawler.php';
 
-//teste
-#$startURL = 'https://olhardigital.com.br/';
-$startURL = 'https://super.abril.com.br/';
+$links = [
+    [ 
+        "site" => "olharDigital", 
+        "link" => "https://olhardigital.com.br/"
+    ],
+    [ 
+        "site" => "sinteressante", 
+        "link" => "https://super.abril.com.br/"
+    ],
+    [ 
+        "site" => "canaltech", 
+        "link" => "https://canaltech.com.br"
+    ],
+];
+
 $depth = 6;
 $username = 'YOURUSER';
 $password = 'YOURPASS';
-$crawler = new crawler($startURL, $depth);
+$crawler = new crawler($links[0]['link'], $depth);
 $crawler->setHttpAuth($username, $password);
 // Exclude path with the following structure to be processed 
 $crawler->addFilterPath('customer/account/login/referer');
